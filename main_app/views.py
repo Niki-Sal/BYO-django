b1258d73063b9b43c9b828bd944ddc9e6f92f000
niki-sal <ne.salehi@gmail.com>
2021-03-23 14:36:17 -0700

    Add plant list page:index.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
 from django.http import HttpResponse, HttpResponseRedirect
+
+# import models
+from .models import Plant
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def plants_index(request):
+    plants = Plant.objects.all()
+    return render(request, 'plants/index.html', { 'plants': plants })
