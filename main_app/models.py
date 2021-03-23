dc832a324d7d3f6ea7fa4d0b5e4b088edfc7dce3
niki-sal <ne.salehi@gmail.com>
2021-03-23 14:19:13 -0700

    Add and Migrate Plant model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    plant = models.CharField(max_length=100)
+    instruction = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
